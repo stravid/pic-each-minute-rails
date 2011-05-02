1756c688ae8f2e97c3d534a5d6e2ea0f1e813ecd
Mathias Paumgarten <mail@mathias-paumgarten.com>
2011-05-02 17:35:23 +0200

    skips authentication on update

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :verify_authenticity_token, :only => [:create, :update]
   
   def new
     @picture = Picture.new
